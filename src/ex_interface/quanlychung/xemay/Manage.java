22e1608644b8f40b797848bafb016c51a46a789b
Hiep <vuxuanhiep1709@gmail.com>
2024-04-16 23:11:59 +0700

    update

--- src/ex_interface/quanlychung/xemay/Manage.java
+++ src/ex_interface/quanlychung/xemay/Manage.java
@@ -1,19 +1,8 @@
 package ex_interface.quanlychung.xemay;
 
 import ex_interface.hanoi.xemay.XeMayHaNoi;
-import ex_interface.xeco.info.XeMay;
-
-import java.util.Scanner;
 
 public class Manage {
-
-    public static int soThuTu(String bienSo) {
-        XeMay xm = new XeMay();
-
-        String[] parts = bienSo.split("-");
-        return Integer.parseInt(parts[1].replaceAll("\\.", ""));
-    }
-
     public static void main(String[] args) {
         XeMayHaNoi xhn = new XeMayHaNoi();
 
